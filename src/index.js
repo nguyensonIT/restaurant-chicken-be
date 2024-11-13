8aead65b7d5626f69843e9f6abaed28e69745804
nguyensonIT <son246504@gmail.com>
2024-11-13 15:47:48 +0700

    fix index file

--- src/index.js
+++ src/index.js
@@ -1,45 +1,34 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
-
-const http = require('http');
-const socketIo = require('socket.io');
 
 require('dotenv').config(); // Đọc biến môi trường từ tệp .env
 
 // Khởi tạo Firebase Admin SDK
 require('./utils/cloudinary');
 
-const route = require("./routes")
-const db = require("./config/db")
-const setupWebSocketServer = require('./websocket/websocket.js');
+const route = require("./routes");
+const db = require("./config/db");
 
-//connect to DB
-db.connect()
+// Kết nối tới DB
+db.connect();
 
-const app = express()
-const server = http.createServer(app); // Tạo HTTP server từ Express
-const io = socketIo(server); // Gắn socket.io vào HTTP server
+const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 // Cấu hình CORS
 app.use(cors());
 
-app.use(express.urlencoded({ limit: '100mb',extended:true}))
-app.use(express.json({ limit: '100mb'}))
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
 
+// HTTP logger
+app.use(morgan('combined'));
 
-//HTTP logger
-app.use(morgan('combined'))
+// Khởi tạo routes
+route(app);
 
-
-//routes init
-route(app)
-
-// Khởi tạo WebSocket server
-setupWebSocketServer(server);
-
-server.listen(PORT, () => {
+// Khởi động server
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
