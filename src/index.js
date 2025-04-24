857d67738cd3021010b662af97ae198f33246802
nguyensonIT <son246504@gmail.com>
2025-04-24 13:10:55 +0700

    adđ prerender to add meta to index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const prerender = require("prerender-node");
 
-require('dotenv').config(); // Đọc biến môi trường từ tệp .env
+require("dotenv").config(); // Đọc biến môi trường từ tệp .env
 
 // Khởi tạo Firebase Admin SDK
-require('./utils/cloudinary');
+require("./utils/cloudinary");
 
 const route = require("./routes");
 const db = require("./config/db");
@@ -19,11 +20,15 @@ const PORT = process.env.PORT || 3000;
 // Cấu hình CORS
 app.use(cors());
 
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json({ limit: '50mb' }));
+// Dùng để thêm thẻ meta khi dán link facebook
+prerender.set("prerenderToken", process.env.PRERENDER_TOKEN);
+app.use(prerender);
+
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 
 // HTTP logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // Khởi tạo routes
 route(app);
